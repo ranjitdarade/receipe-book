8513f7f78873ea707d49c5526717bb6202f15c09
ranjitdarade <130884165+ranjitdarade@users.noreply.github.com>
2025-03-09 16:19:50 +0530

    Add files via upload

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",      // Ensure this file is included
+    "./src/**/*.{js,ts,jsx,tsx}", // Include your component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
